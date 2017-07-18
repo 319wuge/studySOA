cea5759b45b56bf6d123e749c41c0f921f616b0c
tonggen <1799735174@qq.com>
2017-07-18 18:07:13 +0800

    测试日志

--- StudySOAServer/src/test/java/com/wuge/study/soa/AppTest.java
+++ StudySOAServer/src/test/java/com/wuge/study/soa/AppTest.java
@@ -1,38 +1,16 @@
 package com.wuge.study.soa;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.wuge.study.soa.service.sale.OrderServiceImpl;
+import com.wuge.study.soa.sale.IOrderService;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @org.junit.Test
+    public void testLogger() {
+        IOrderService service = new OrderServiceImpl();
+        service.testLogger();
     }
 }
